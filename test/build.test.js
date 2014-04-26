60c85495eb0b07c76c4acb610f09b5784b19c83e
Mike McNeil <mike@balderdash.co>
2014-04-26 12:07:35 -0500

    Added build test and usage validation.

---
+++ test/build.test.js
@@ -0,0 +1,39 @@
+var assert = require('assert');
+var fsx = require('fs-extra');
+var Compiler = require('../lib/Compiler');
+
+
+
+describe('Compiler.prototype.build', function () {
+  
+  var compiler = new Compiler();
+
+  // Make sure a temporary directory exists
+  // and empty it out if it does already
+  var tmpDirPath = require('path').resolve(__dirname,'../.tmp');
+  try {
+    fsx.removeSync(tmpDirPath);
+  }
+  catch(e){}
+  try {
+    fsx.mkdirSync(tmpDirPath);
+  }
+  catch (e){}
+
+  it('should not throw or error out when passed a valid array of build instructions', function (done) {
+    compiler.build([{
+
+    }], function whenFinished (err, metadata){
+      if (err) return done(err);
+      done();
+    });
+  });
+  // it('should create HTML markup files in the expected destination directory', function (done) {
+  //   compiler.build([{
+
+  //   }], function whenFinished (err, metadata){
+  //     if (err) return done(err);
+  //     done();
+  //   });
+  // });
+});
