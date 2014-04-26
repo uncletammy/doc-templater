16dfc62a35ab89e7d9f5bd67aec31e28426a4a66
Mike McNeil <mike@balderdash.co>
2014-04-26 12:16:16 -0500

    Wipe tmp files before and after each tests.

--- test/build.test.js
+++ test/build.test.js
@@ -10,22 +10,35 @@ describe('Compiler.prototype.build', function () {
 
   // Make sure a temporary directory exists
   // and empty it out if it does already
-  var tmpDirPath = require('path').resolve(__dirname,'../.tmp');
-  try {
-    fsx.removeSync(tmpDirPath);
+  // (this library will create one anyway, but since we also need
+  // .tmp for our test output, we want to make sure it exists
+  // to eliminate variables from test failures)
+  afterEach(wipeTmpFiles);
+  beforeEach(wipeTmpFiles);
+  function wipeTmpFiles () {
+    var tmpDirPath = require('path').resolve(__dirname,'../.tmp');
+    try {
+      fsx.removeSync(tmpDirPath);
+    }
+    catch(e){}
+    try {
+      fsx.mkdirSync(tmpDirPath);
+    }
+    catch (e){}
   }
-  catch(e){}
-  try {
-    fsx.mkdirSync(tmpDirPath);
-  }
-  catch (e){}
+
 
   it('should not throw or error out when passed a valid array of build instructions', function (done) {
+    
+    // This test isn't "slow" unless it takes longer than 5 seconds
+    this.slow(5000);
+
     compiler.build([{
       docsGitRepo: 'git://github.com/balderdashy/sails-docs-guides.git',
       parsedTemplatesDirectory: '.tmp/foo'
     }], function whenFinished (err, metadata){
       if (err) return done(err);
+      assert(fsx.existsSync('.tmp/foo'));
       done();
     });
   });
