cf87c110997f9529cd681ddccc297385a0c1ad8f
Mike McNeil <mike@balderdash.co>
2014-07-25 03:19:35 -0500

    Fix up test script a bit

--- TEST_SCRIPT.js
+++ TEST_SCRIPT.js
@@ -1,8 +1,11 @@
-k = require('node-machine').require('./lib/compile-markdown-tree-from-remote-git-repo');
-k.configure({
-
-})
-.exec(function (e,r) {
+k = require('./lib/compile-markdown-tree-from-remote-git-repo');
+k({
+  remote: 'git://github.com/mikermcneil/machinepack-fs.git',
+  remoteSubPath: 'lib/',
+  cachePath: '/code/sandbox/doctemplatertest/foo/bar/cache/',
+  htmlDirPath: '/code/sandbox/doctemplatertest/foo/bar/html',
+  jsMenuPath: '/code/sandbox/doctemplatertest/foo/bar.jsmenu'
+}, function (e,r) {
   if (e) {console.log('ERROR:\n',e);}
   else console.log('RESULT:\n',r);
 });
