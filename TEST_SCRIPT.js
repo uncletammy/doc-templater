cd4f94a7ee9b7faac9f0386605e005165a83a98d
Mike McNeil <mike@balderdash.co>
2014-07-25 03:21:25 -0500

    Fix machinepack requires

--- TEST_SCRIPT.js
+++ TEST_SCRIPT.js
@@ -1,6 +1,6 @@
 k = require('./lib/compile-markdown-tree-from-remote-git-repo');
 k({
-  remote: 'git://github.com/mikermcneil/machinepack-fs.git',
+  remote: 'git@github.com:mikermcneil/machinepack-fs.git',
   remoteSubPath: 'lib/',
   cachePath: '/code/sandbox/doctemplatertest/foo/bar/cache/',
   htmlDirPath: '/code/sandbox/doctemplatertest/foo/bar/html',
