c1199976b279e85661041a0e0109022f05b663b5
Mike McNeil <mike@balderdash.co>
2016-01-11 10:43:42 -0600

    Bump mp-git dependency and remove deprecated option being passed through to mp-git.

--- SECOND_TEST_SCRIPT.js
+++ SECOND_TEST_SCRIPT.js
@@ -11,8 +11,9 @@ k([
 {
   remote: 'git@github.com:mikermcneil/machinepack-fs.git',
   remoteSubPath: '',
-  // htmlDirPath: '/code/sandbox/doctemplatertest/foo/bar2/html',
-  // cachePath: '/code/sandbox/doctemplatertest/foo/bar/cache2',
+  branch: 'experimental',
+  htmlDirPath: '/code/sandbox/doctemplatertest/foo/bar2/html',
+  cachePath: '/code/sandbox/doctemplatertest/foo/bar/cache2',
 }], function (e,r) {
   if (e) {console.log('ERROR:\n',require('util').inspect(e, false, null));}
   else console.log('RESULT:\n',require('util').inspect(r, false, null));
